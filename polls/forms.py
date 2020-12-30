92267cd143fbdc4538b8147c21b3efedaa00c0ca
Kosi <keloasuzu@yahoo.com>
2020-12-30 14:03:14 +0100

    More bug fixes

--- polls/forms.py
+++ polls/forms.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     required_css_class = 'form-control'
     username = forms.CharField(label="username", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'username'}))
-    password = forms.CharField(label="password", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'password'}))
+    password = forms.CharField(label="password", max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'password'}))
 
 
 class RegisterForm(forms.ModelForm):
     username = forms.CharField(label="username", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'username'}))
-    password = forms.CharField(label="password", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'password'}))
+    password = forms.CharField(label="password", max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'password'}))
     email = forms.CharField(label="email", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'email'}))
     first_name = forms.CharField(label="firstname", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'firstname'}))
     last_name = forms.CharField(label="lastname", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'lastname'}))
